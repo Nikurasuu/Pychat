f669a27ff448e0d32f55902d60ba89433f729518
Niklas Malkusch <56842703+Nikurasuu@users.noreply.github.com>
2020-07-20 21:46:50 +0200

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,6 +1,38 @@
 import mysql.connector
 import time
 
+def nachrichten_auslesen():
+    #Nachrichten auslesen und anzeigen
+        print("Aktuelle Nachrichten: ")
+        print("")
+
+        mycursor = mydb.cursor()
+        mycursor.execute("SELECT * FROM chatroom1")
+
+        myresult = mycursor.fetchall()
+        
+        print(myresult)
+
+        for row in myresult:
+            print("(" + str(row[0]) + ") " + row[1] + ": " + row[2])
+            time.sleep(0.05)
+
+        print("")
+
+def passwort_überprüfen():
+    passwort = input("Passwort eingeben: ")
+    
+    mycursor = mydb.cursor()
+    sqlFormula = "SELECT password FROM users WHERE username = '%s'"
+    packet = (username)
+
+    mycursor.execute(sqlFormula, packet)
+
+    myresult = mycursor.fetchall()
+
+    print(myresult)
+
+
 mysql_server = input("MySQL Server address: ")
 
 mydb = mysql.connector.connect(
@@ -14,6 +46,7 @@ print(mydb)
 
 username = input("Bitte Nutzernamen eingeben: ")
 print("Hallo " + username + "!")
+passwort_überprüfen()
 
 while True:
     modeinput = input("1: Nachrichten lesen, 2: Nachricht schreiben, 3: Nachrichten löschen ")
@@ -21,23 +54,10 @@ while True:
     try:
         mode = int(modeinput)
     except:
-        mode = 10
+        mode = 4
 
     if mode == 1:
-        #Nachrichten auslesen und anzeigen
-        print("Aktuelle Nachrichten: ")
-        print("")
-
-        mycursor = mydb.cursor()
-        mycursor.execute("SELECT * FROM chatroom1")
-
-        myresult = mycursor.fetchall()
-        
-        for row in myresult:
-            print("(" + str(row[0]) + ") " + row[1] + ": " + row[2])
-            time.sleep(0.05)
-
-        print("")
+        nachrichten_auslesen()
 
     if mode == 2:
         print("")
@@ -55,19 +75,7 @@ while True:
         print("")
 
     if mode == 3:
-        #Nachrichten auslesen und anzeigen
-        print("Aktuelle Nachrichten: ")
-        print("")
-
-        mycursor = mydb.cursor()
-        mycursor.execute("SELECT * FROM chatroom1")
-
-        myresult = mycursor.fetchall()
-        
-        for row in myresult:
-            print("(" + str(row[0]) + ") " + row[1] + ": " + row[2])
-
-        print("")
+        nachrichten_auslesen()
 
         nachricht_nummer = input("Welche Nachricht? ")
         try:
